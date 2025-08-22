3febc7d25a054b44c7fd1db5d8d0775633996cd8
Albert Szupszynski <albert733844@gmail.com>
2025-08-22 15:21:31 +0100

    first merge

--- src/app/about/choosegame/page.tsx
+++ src/app/about/choosegame/page.tsx
@@ -6,6 +6,9 @@ export default function ChooseGamePage() {
       </h1>
     <img src="/sword.jpg" alt="Sword and Shield" className="mt-4 w-100 h-auto " />
       <h2>Pokémon Sword / Pokémon Shield</h2>
+      <p className="mt-2">
+        Pokémon Sword and Shield are the first mainline Pokémon games for the Nintendo Switch. They introduce players to the Galar region, inspired by the United Kingdom, and feature new Pokémon, mechanics, and a fresh storyline. The games also introduced Dynamax and Gigantamax forms, adding a new layer of strategy to battles.
+      </p>
     </div>
   );
 }
