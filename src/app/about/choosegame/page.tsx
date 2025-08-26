6a1ad4cf7585abf991f360c1beb25f5e7b0479c1
Albert Szupszynski <albert733844@gmail.com>
2025-08-26 11:03:26 +0100

    added new page for pokemon shield/sword

--- src/app/about/choosegame/page.tsx
+++ src/app/about/choosegame/page.tsx
@@ -1,10 +1,19 @@
+"use client";
+import { useRouter } from 'next/navigation';
+
 export default function ChooseGamePage() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/about/swordshield');
+  };
+
   return (
-    <div>
+    <div onClick={handleClick} className="cursor-pointer">
       <h1 className="text-3xl text-center">
         Choose Game
       </h1>
-    <img src="/sword.jpg" alt="Sword and Shield" className="mt-4 w-100 h-auto " />
+      <img src="/sword.jpg" alt="Sword and Shield" className="mt-4 w-100 h-auto " />
       <h2>Pokémon Sword / Pokémon Shield</h2>
     </div>
   );
---
+++ src/app/about/swordshield/page.tsx
@@ -0,0 +1,9 @@
+export default function ChooseGamePage() {
+  return (
+    <div>
+      <h1 className="text-3xl text-center">
+        Choose Pokémon
+      </h1>
+    </div>
+  );
+}
